be76b795d32df7dc23ddb613426b6e5a07a02f50
Janrylle Espinosa <98929444+JanRealAcc@users.noreply.github.com>
2023-09-23 22:52:14 +0800

    Update flag_2.ino
    These comments explain each part of the flag_2() function:
    
    char buffer[25]; declares a character array to store text data.
    
    The function flag_2() checks if Ultrasonic sensor #2 detects an object within a specified time frame, based on certain conditions:
    
    object1_Detected must be true.
    object2_Detected must be false.
    The time since object1 detection (millis() - object1_DetectionTime) must be less than or equal to active_Delay.
    Inside this condition:
    
    int distance2 = measureDistance(trigPin2, echoPin2); measures the distance using Ultrasonic sensor #2.
    Serial.print("F2 - "); is used for monitoring and prints "F2 - " to indicate Flag 2 data.
    Serial.println(distance2); prints the measured distance to the Serial monitor.
    receiveMessage(); calls a function to receive a message, but its purpose isn't provided in the code.
    If the measured distance2 is less than 15 units:
    
    A message is sent twice using sendMessage() (function purpose not provided).
    There's a delay of 200 milliseconds.
    A traffic sensor is activated (function purpose not provided), followed by a 50-millisecond delay.
    A text message is formatted using snprintf and stored in the buffer variable.
    The formatted message is sent to the Serial monitor using sendSerial(buffer) (function purpose not provided).
    A pedestrian trigger event is initiated (function purpose not provided).
    The traffic sensor is deactivated (function purpose not provided).

--- flag_2.ino
+++ flag_2.ino
@@ -1,35 +1,31 @@
-char buffer[25];
+char buffer[25]; // Declare a character array to store text data.
 
-void flag_2(){
-// ***Check if Ultrasonic sensor #2 detects an object within the specified time***
+void flag_2() {
+  // Check if Ultrasonic sensor #2 detects an object within the specified time
   if (object1_Detected && !object2_Detected && (millis() - object1_DetectionTime <= active_Delay)) {
-    int distance2 = measureDistance(trigPin2, echoPin2);
-    Serial.print("F2 - ");
-    Serial.println(distance2);
-    receiveMessage();  
+    int distance2 = measureDistance(trigPin2, echoPin2); // Measure the distance using Ultrasonic #2.
+    Serial.print("F2 - "); // Print "F2 - " to indicate Flag 2 data.
+    Serial.println(distance2); // Print the measured distance to the Serial monitor.
+    receiveMessage(); // Receive a message (function purpose not provided).
 
-
-    if (distance2 < 15) {
-      for(int i=0; i<2 ; i++){
-        sendMessage();
+    if (distance2 < 15) { // Check if the measured distance is less than 15 units.
+      for (int i = 0; i < 2; i++) {
+        sendMessage(); // Send a message twice (function purpose not provided).
       }
 
-      delay(200);
+      delay(200); // Delay for 200 milliseconds.
 
-      activateTrafficSensor();
-      delay(50);
+      activateTrafficSensor(); // Activate a traffic sensor (function purpose not provided).
+      delay(50); // Delay for 50 milliseconds.
 
-
+      // Format a text message using snprintf and store it in the 'buffer' variable.
       snprintf(buffer, sizeof(buffer), "Duration_Execute: %d", cycleTime_Duration);
-      sendSerial(buffer);
 
-      pedestrianTrigger();
-      deactivateTrafficSensor();
+      // Send the formatted message to the Serial monitor.
+      sendSerial(buffer); // Function sendSerial() is used to send the message.
 
-
-
-
+      pedestrianTrigger(); // Trigger a pedestrian event (function purpose not provided).
+      deactivateTrafficSensor(); // Deactivate the traffic sensor (function purpose not provided).
     }
   }
-
 }
