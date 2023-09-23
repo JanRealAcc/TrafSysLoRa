fc40d7fb4a2c0d2f7f71149e4d11963a9e38f8ce
Janrylle Espinosa <98929444+JanRealAcc@users.noreply.github.com>
2023-09-23 22:53:00 +0800

    Update flag_Timeout.ino
    These comments explain each part of the flag_Timeout() function:
    
    The function flag_Timeout() checks if a timeout occurred without detecting an object with Ultrasonic sensor #2, based on certain conditions:
    
    object1_Detected must be true.
    object2_Detected must be false.
    The time since object1 detection (millis() - object1_DetectionTime) must be greater than active_Delay.
    If the conditions are met:
    
    The LED for pedestrian green (pedGreen) is turned off.
    The original LED for pedestrian red (pedRed) is turned on.
    The LED for vehicle green (vehicleGreen) is turned off.
    The original LED for vehicle red (vehicleRed) is turned on.
    A message is printed to the Serial monitor, indicating a timeout because an object was not detected by Ultrasonic sensor #2 within the specified time.
    
    Finally, variables and conditions are reset to prepare for the next cycle:
    
    object1_Detected is set to false.
    object2_Detected is set to false.

--- flag_Timeout.ino
+++ flag_Timeout.ino
@@ -1,17 +1,23 @@
-void flag_Timeout(){
-// Check if timeout occurred without detecting an object with Ultrasonic sensor #2
+void flag_Timeout() {
+  // Check if a timeout occurred without detecting an object with Ultrasonic sensor #2
   if (object1_Detected && !object2_Detected && (millis() - object1_DetectionTime > active_Delay)) {
-    digitalWrite(pedGreen, TURNOFF); // Turn off the new LED
-    digitalWrite(pedRed, TURNON); // Turn on the original LED
-    digitalWrite(vehicleGreen, TURNON); // Turn off the new LED
+    // Turn off the new LED for pedestrian green
+    digitalWrite(pedGreen, TURNOFF);
+
+    // Turn on the original LED for pedestrian red
+    digitalWrite(pedRed, TURNON);
+
+    // Turn off the new LED for vehicle green
+    digitalWrite(vehicleGreen, TURNON);
+
+    // Turn on the original LED for vehicle red
     digitalWrite(vehicleRed, TURNOFF);
 
+    // Print a message indicating a timeout
     Serial.println("Timeout: Object not detected by Ultrasonic #2 within the specified time");
 
     // Reset variables and conditions for the next cycle
     object1_Detected = false;
     object2_Detected = false;
   }
-
-  
-}  
+}
