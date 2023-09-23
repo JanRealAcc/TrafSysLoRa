e1c6627bf1b15ca73b647525cb8ec53f1d22a296
Janrylle Espinosa <98929444+JanRealAcc@users.noreply.github.com>
2023-09-23 22:51:23 +0800

    Update flag_1.ino
    These comments explain each section of the flag_1() function:
    
    The first if condition checks whether object1_Detected is false, ensuring that the code inside this block runs only if object1 hasn't been detected yet.
    
    int distance1 = measureDistance(trigPin1, echoPin1); measures the distance using Ultrasonic sensor #1 and stores it in the distance1 variable.
    
    Serial.print("F1 - "); is used for debugging or monitoring and prints "F1 - " to indicate that the following data is related to Flag 1.
    
    Serial.println(distance1); prints the measured distance to the Serial monitor.
    
    receiveMessage(); calls a function to receive a message, but the purpose of this function isn't explained in the provided code.
    
    The second if condition checks if the measured distance1 is less than 15 units. If it is, it means an object is detected within the specified range.
    
    Inside this condition:
    
    object1_Detected is set to true, indicating that an object has been detected.
    object1_DetectionTime records the current time in milliseconds using millis() to keep track of when the object was detected.
    Finally, Serial.println("Object detected by Ultrasonic #1"); prints a message to the Serial monitor, indicating that an object has been detected by Ultrasonic sensor #1.

--- flag_1.ino
+++ flag_1.ino
@@ -1,21 +1,17 @@
-
-
-void flag_1(){
-  if (!object1_Detected) {
-    int distance1 = measureDistance(trigPin1, echoPin1);
+void flag_1() {
+  if (!object1_Detected) { // Check if object1 has not been detected yet.
+    int distance1 = measureDistance(trigPin1, echoPin1); // Measure the distance using Ultrasonic #1.
 
     // Display on Serial-monitor
-    Serial.print("F1 - ");
-    Serial.println(distance1);
-    receiveMessage();
+    Serial.print("F1 - "); // Print "F1 - " to indicate Flag 1 data.
+    Serial.println(distance1); // Print the measured distance to the Serial monitor.
+    receiveMessage(); // Receive a message (function purpose not provided).
 
+    if (distance1 < 15) { // Check if the measured distance is less than 15 units.
+      object1_Detected = true; // Set object1 detection status to true.
+      object1_DetectionTime = millis(); // Record the detection time in milliseconds.
 
-    if (distance1 < 15) {
-      object1_Detected = true;
-      object1_DetectionTime = millis();
-
-      Serial.println("Object detected by Ultrasonic #1");
-      
+      Serial.println("Object detected by Ultrasonic #1"); // Print a message indicating object detection.
     }
   }
 }
